d1139a466a02ac2512abd2f94e62eac348e36373
janine714 <138150214+janine714@users.noreply.github.com>
2026-01-30 12:43:52 +0000

    Deploying to gh-pages from @ janine714/janine714.github.io@995747743f907e263c45a96c9450ccf4bdc21822 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -420,10 +420,7 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "projects-careaqa-a-cardiac-and-respiratory-audio-question-answering-model-for-open-ended-diagnostic-reasoning",
+            },},{id: "projects-careaqa-a-cardiac-and-respiratory-audio-question-answering-model-for-open-ended-diagnostic-reasoning",
           title: 'CaReAQA: A Cardiac and Respiratory Audio Question Answering Model for Open-Ended Diagnostic Reasoning...',
           description: "A question-answering model designed to enable large language models to perform diagnostic reasoning over auscultation audio recordings from real-world clinical data.",
           section: "Projects",handler: () => {
